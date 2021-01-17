564701803d67bde7fbbb6c27a3f81f11646875f6
CSWILIK <cswilik@gmail.com>
2021-01-16 22:09:05 -0500

    commit tree 6f17f4212c4bc958a828f64f99e6a5981757f0c0

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -7,12 +7,35 @@ import { CATEGORIES, TASKS } from "../data";
 
 
 function App() {
+  const [ tasksArr, setTasks] = useState(TASKS)
+  const [selectCat, setSeletCat] = useState("All")
+
+ function handleDelete(taskToDelete) {
+    const newArr = tasksArr.filter(task => 
+      task.text !== taskToDelete
+    )
+    setTasks(newArr)
+  }
+
+  function onTaskFormSubmit(newTask) {
+    const newTasks = [...tasksArr, newTask]
+
+    setTasks(newTasks)
+  }
+  
+
+  const filteredArr = tasksArr.filter(task => {
+    return selectCat === "All" || task.category === selectCat
+    })
+
+  
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList tasks ={TASKS}/>
+      <CategoryFilter categories={CATEGORIES} selectCat = {selectCat} setSelectCat={setSeletCat}/>
+      <NewTaskForm categories= {CATEGORIES} onTaskFormSubmit = {onTaskFormSubmit} />
+      <TaskList tasks ={filteredArr} onDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectCat, setSelectCat}) {
+
+
+  const categoryElement = categories.map(cat => {
+    const className = cat === selectCat ? "selected" : null;
+    return (<button className={className} key={cat} onClick={() => setSelectCat(cat)}>{cat} </button>)
+  })
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryElement}
     </div>
   );
 }
